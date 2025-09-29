42c54b61440df22f12420c4a369c7664670a2df6
Govind Kumawat <kumawatgovind222@gmail.com>
2025-09-29 14:12:26 +0530

    updated: scss structure  and latest version of dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import topLevelAwait from "vite-plugin-top-level-await";
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/scss/global.scss";`,
+        additionalData: `@import "@/assets/scss/mandatoryimports.scss";`,
       },
     },
   },
   server: {
-    host: true
+    host: true,
   },
-  base: './'
-})
+  base: './',
+});
